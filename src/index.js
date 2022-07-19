f31ccadb060a7e28d9c07dbf16ef2e4faac96b31
Lina Alvarado <linamariaalvaradopallares@gmail.com>
2022-07-19 18:59:41 -0500

    agregando cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+let numeroTarjeta="";
+
+function captura(){
+numeroTarjeta = document.getElementById("numTarjeta").value;
+let numArray= numeroTarjeta.split('').reverse();
+let suma = 0;
+let num = 0;
+for (let i= 0; i< numArray.length; i++){
+  if (i % 2 === 1){
+    num = Number( numArray[i] * 2);
+    if (num >= 10){
+      num = (num-10) + 1;
+    }
+  } else{
+    num = Number(numArray[i]);
+  }
+} suma += num;
+}
+suma % 10 === 0? true : false;
+return (suma)
+}
+
+let botonPagar = document.getElementById("botonPagar")
+botonPagar.addEventListener('click',captura);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,22 @@
-const validator = {
-  // ...
-};
+// const validator = {
+// //   isValid:(numeroTarjeta) =>{
+// //     let numArray= numeroTarjeta.split('').reverse();
+// //     let suma = 0;
+// //     let num = 0;
+// //     for (let i= 0; i< numArray.length; i++){
+// //       if (i % 2 === 1){
+// //         num = Number( numArray[i] * 2);
+// //         if (num >= 10){
+// //           num = (num-10) + 1;
+// //         }
+// //       } else{
+// //         num = Number(numArray[i]);
+// //       }
+// //     } suma += num;
+// //   }
+// //   suma % 10 === 0? true : false;
+// //   return (suma)
+// // };
 
-export default validator;
+// export default validator;
+
