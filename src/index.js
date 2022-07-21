ae0fbd59e0f17e4faa9a0a95f4f1055652a11713
Lina Alvarado <linamariaalvaradopallares@gmail.com>
2022-07-20 19:18:52 -0500

    Modificaciones en funciones

--- src/index.js
+++ src/index.js
@@ -2,26 +2,22 @@ import validator from './validator.js';
 
 const botonPagar = document.getElementById("botonPagar");
 
-botonPagar.addEventListener("click", () => { // instrucciones al dar click al boton 
-  const numTarjeta = document.getElementById("numTarjeta");
-  const isValid = validator.isValid(numTarjeta.value);
-  const maskify = validator.maskify(numTarjeta.value);
-  console.log(maskify);
+// instrucciones al dar click al boton 
+botonPagar.addEventListener("click", () => { 
+const numTarjeta = document.getElementById("numTarjeta");
+const isValid = validator.isValid(numTarjeta.value);
+const maskify = validator.maskify(numTarjeta.value);
 
-  // ALERTAS
-  if (isValid === true) {
+
+// ALERTAS
+if (isValid === true) {
     alert("La tarjeta de crédito " + maskify + "  es válida"); 
-  } else {
+} else {
     alert("La tarjeta de crédito " + maskify + " no es válida");
-  }
-
-
-
-
+}
 
 });
 
-console.log(validator);
 
 
 
